e3858b397c1ae38bf924b6560878a7119c87be16
Ajaybhukya <ajaykumarbhukya511@gmail.com>
2025-07-23 17:17:36 +0530

    my commit

--- src/main/java/com/intranet/controller/TimeSheetController.java
+++ src/main/java/com/intranet/controller/TimeSheetController.java
@@ -1,24 +1,26 @@
 package com.intranet.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.intranet.entity.TimeSheet;
-import com.intranet.entity.TimeSheetEntry;
 import com.intranet.service.TimeSheetService;
 
 @RestController
-@RequestMapping("/timesheet")
+@RequestMapping("/api/timesheets")
 public class TimeSheetController {
-    
+
     @Autowired
     private TimeSheetService timeSheetService;
 
-
-    @PostMapping("/create/{userId}")
-    public TimeSheet createTimesheet(Long userId) {
-         return timeSheetService.createTimesheet(userId, new TimeSheet());
+    @PostMapping("/create")
+    public ResponseEntity<TimeSheet> createTimeSheet(@RequestBody TimeSheet timesheet) {
+        TimeSheet savedTimeSheet = timeSheetService.createTimeSheet(timesheet);
+        return new ResponseEntity<>(savedTimeSheet, HttpStatus.CREATED);
     }
 }
--- src/main/java/com/intranet/service/TimeSheetService.java
+++ src/main/java/com/intranet/service/TimeSheetService.java
@@ -4,15 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.intranet.entity.TimeSheet;
+import com.intranet.entity.TimeSheetEntry;
 import com.intranet.repository.TimeSheetRepo;
 
 @Service
 public class TimeSheetService {
     @Autowired
     private TimeSheetRepo timeSheetRepo;
+    public TimeSheet createTimeSheet(TimeSheet timesheet) {
+        if (timesheet.getEntries() != null) {
+            for (TimeSheetEntry entry : timesheet.getEntries()) {
+                entry.setTimesheet(timesheet);
+            }
+        }
 
-    public TimeSheet createTimesheet(Long userId, TimeSheet timesheet) {
-        timesheet.setUserId(userId);
-        return timeSheetRepo.save(timesheet);
-    }   
+        return timeSheetRepo.save(timesheet); // saves timesheet and entries due to CascadeType.ALL
+    }
 }
