70bd59079f6e7f95923dffaf2612a5b2dc82b89c
rohitlingarker <rohitlingerker@gmail.com>
2025-07-24 16:01:43 +0530

    Update Timesheet API

--- src/main/java/com/intranet/entity/TimeSheetApproval.java
+++ src/main/java/com/intranet/entity/TimeSheetApproval.java
@@ -1,6 +1,5 @@
 package com.intranet.entity;
 import java.time.LocalDateTime;
-import org.hibernate.annotations.CreationTimestamp;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,7 +7,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
